5a8fe46cfa8c0961e522c86cf1e4c8f39ed00cdd
PaulBalan1 <56250869+PaulBalan1@users.noreply.github.com>
2020-03-03 14:38:26 +0100

    test2

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -24,5 +24,6 @@ public class MyApplication extends Application
     ViewHandler view = new ViewHandler(model);
     view.start(primaryStage);
     /////////////////////////////////////////////////
+    //test3
   }
 }
